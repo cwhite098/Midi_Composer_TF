8922d8683c9044045ea15a1eec0dcddce3b80324
Chris <gd19031@bristol.ac.uk>
2021-09-24 13:47:39 +0100

    Added button class and temporarilly added a play and pause button for the piano roll and the music playback

--- GUI.py
+++ GUI.py
@@ -6,6 +6,38 @@ import scipy.ndimage
 from mido import MidiFile
 
 
+class button():
+    # https://www.youtube.com/watch?v=4_9twnEduFA
+    def __init__(self, color, x,y,width,height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self,win,outline=None):
+        #Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+            
+        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 40)
+            text = font.render(self.text, 1, (0,0,0))
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+            
+        return False
+
+
+
 
 def process_midi(midi_file):
 
@@ -56,33 +88,53 @@ def run_GUI(piano_roll, size_x, size_y):
     max_time = mid.length
     print(max_time)
 
-
-
-    display = pygame.display.set_mode((500, 700))
+    display = pygame.display.set_mode((540, 700))
     display.fill(WHITE)
     roll_display = pygame.Surface((size_x, 200))
     
     surf = pygame.surfarray.make_surface(piano_roll)
 
-    roll_speed = size_y/(max_time * FPS)
+    roll_speed = 0
     
-
     running = True
     y_coord = 0
 
-    pygame.mixer.music.play()
+    play_button = button((100,100,100), 10, 450, 100, 50,'Play')
+    pause_button = button((100,100,100), 500, 450, 100, 50,'Pause')
+    mouse_pos = pygame.mouse.get_pos()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.isOver(mouse_pos):
+                    pygame.mixer.music.play()
+                    roll_speed = size_y/(max_time * FPS)
+                if pause_button.isOver(mouse_pos):
+                    pygame.mixer.music.stop()
+                    roll_speed = 0
         
+            if event.type == pygame.MOUSEMOTION:
+                if play_button.isOver(mouse_pos):
+                    play_button.color = (150,150,150)
+                else:
+                    play_button.color = (100,100,100)
+                if pause_button.isOver(mouse_pos):
+                    pause_button.color = (150,150,150)
+                else:
+                    pause_button.color = (100,100,100)
+
         display.blit(roll_display,((500-size_x)/2 ,20 ))
         roll_display.blit(surf, (0,y_coord-size_y+200))
         y_coord += roll_speed
         print(y_coord)
 
+        mouse_pos = pygame.mouse.get_pos()
 
+        play_button.draw(display)
+        pause_button.draw(display)
         pygame.display.update()
         clock.tick(FPS)
 
